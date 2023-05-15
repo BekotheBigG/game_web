2e8096f3b1f8684b3c718df258d3e4b2a3d13318
Beko <94633878+BekotheBigG@users.noreply.github.com>
2023-05-15 23:36:47 +0300

    add game new version

--- src/pages/game.tsx
+++ src/pages/game.tsx
@@ -3,10 +3,10 @@ import { Unity, useUnityContext } from "react-unity-webgl";
 
 export default function Game() {
   const { unityProvider } = useUnityContext({
-    loaderUrl: "build/WebGl.loader.js",
-    dataUrl: "build/WebGl.data",
-    frameworkUrl: "build/WebGl.framework.js",
-    codeUrl: "build/WebGl.wasm",
+    loaderUrl: "build/WEBGL.loader.js",
+    dataUrl: "build/WEBGL.data",
+    frameworkUrl: "build/WEBGL.framework.js",
+    codeUrl: "build/WEBGL.wasm",
   });
 
   return <Unity style={{ width:"100%", justifySelf:"center", alignSelf:"center"}} unityProvider={unityProvider} />;
